5a2888142594968bb310ba4ec356e677964beb5b
suru <garudahorror25@gmail.com>
2024-06-21 10:53:35 -0400

    <build> modified the way the page month is supposed to render to be exactly the way it is supposed to be

--- src/atoms/todo_month.js
+++ src/atoms/todo_month.js
@@ -1,3 +1,14 @@
-const todoMonth = () => {}
+import {renderTodoLists} from "../utils";
+
+const main = renderTodoLists();
+
+const todoMonth = () => {
+  const month = document.createElement('div');
+  month.classList.add('feature-list');
+
+  main.renderList(month, 'month');
+
+  return month;
+}
 
 export default todoMonth;

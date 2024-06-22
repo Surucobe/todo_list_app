57c8184593dc0490046c869f571131e9b5904225
suru <garudahorror25@gmail.com>
2024-06-22 15:55:24 -0400

    <build> too many modifications

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const main = renderTodoLists()
 
 const app = document.getElementById('app');
 
-import toDoContainer from "./src/components/feature-list.js";
+import toDoContainer from "./src/components/todo-container.js";
 import sidebar from "./src/components/sidebar.js"
 
 import './styles/global.css';
 
 app.appendChild(sidebar(main.changeList));
-app.appendChild(toDoContainer());
+//todoContainer needs to receive a paramtere to decided what is it going to render
+console.log(app)
+main.changeList(app, toDoContainer);
+console.log(app)

836d02751c3ffa32c120694ac91486758bc0af14
suru <garudahorror25@gmail.com>
2024-06-24 12:34:18 -0400

    <refactor> refactored the way element are being render to make it more clean and simple to understand

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,9 @@
 import { Collection } from '../utils';
 import toDoContainer from './todo-container';
 
+import userProfile from '../atoms/sidebar/user';
+import list from '../atoms/sidebar/list';
+
 //reserved for the import on the styles
 import '../../styles/sidebar.css';
 
@@ -10,34 +13,16 @@ const sidebar = (callback) => {
   const sidebar = document.createElement('div');
   sidebar.classList.add('sidebar-sections');
 
-  const user = document.createElement('div');
-  user.classList.add('user-container');
-  const userImage = new Image();
-  userImage.src = userPicture
-  user.appendChild(userImage);
-  const userSpan = document.createElement('span');
-  userSpan.innerHTML = `small text`;
-  user.appendChild(userSpan);
+  const user = userProfile(userPicture, 'Suru');
   sidebar.appendChild(user);
-
+  
   const mainList = document.createElement('div');
   mainList.classList.add('main-list');
+  
   const header = document.createElement('h2');
   header.innerText = 'My lists';
-  const sidebarItemListContainer = document.createElement('ul');
-  Collection.getTitles().forEach(title => {
-    const newTitle = document.createElement('li');
-    newTitle.innerText = title
 
-    newTitle.addEventListener('click', (e) => {
-    // callback(title, console.log('we got here'))
-    callback(toDoContainer(title))
-  })
-
-    sidebarItemListContainer.appendChild(newTitle);
-  });
-  
-  // console.log(Collection.getTitles());
+  const sidebarItemListContainer = list(Collection.getTitles(), callback);
 
   mainList.appendChild(header);
   mainList.appendChild(sidebarItemListContainer);

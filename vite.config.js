571c650fcfd6a1bd418edad755658146521e4eba
suru <garudahorror25@gmail.com>
2024-06-25 14:04:12 -0400

    <test> quick test commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://github.com/Surucobe/todo_list_app/"
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "https://github.com/Surucobe/todo_list_app/"
+// })
